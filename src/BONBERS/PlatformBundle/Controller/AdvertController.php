452dfa30d09a6cb753472861a422d7504cf396d4
bonbers <guillot.romain@ymail.com>
2017-10-23 14:39:26 +0200

    TP progress

--- src/BONBERS/PlatformBundle/Controller/AdvertController.php
+++ src/BONBERS/PlatformBundle/Controller/AdvertController.php
@@ -2,95 +2,127 @@
 
 namespace BONBERS\PlatformBundle\Controller;
 
+use BONBERS\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
+    
     public function indexAction($page)
     {
-        $listAdverts = array(
-      array(
-        'title'   => 'Recherche développpeur Symfony',
-        'id'      => 1,
-        'author'  => 'Alexandre',
-        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Mission de webmaster',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title' => 'Offre de stage webdesigner',
-        'id' => 3,
-        'author'  => 'Mathieu',
-        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-        'date'    => new \Datetime())
-    );
-       if ($page < 1 ) {
+              if ($page < 1 ) {
            throw new NotFoundHttpException('Page"'.$page.'"inexistante.');
        }
+       
+       // On fixe le nombre d'annonces par page à 3
+       $nbPerPage = 3;
+       
+       // On récupère la liste de toute les annonces avec findAll()
+       
+        $listAdverts = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('BONBERSPlatformBundle:Advert')
+                ->getAdverts()
+                ;
+       
+       // On récupère notre objet Paginator
+       $listAdverts = $this->getDoctrine()
+               ->getManager()
+               ->getRepository('BONBERSPlatformBundle:Advert')
+               ->getAdverts($page, $nbPerPage)
+               ;
+       
+       // On calcul le nombre total de pages qui retourne le nombre total d'annonces
+       $nbPages = ceil(count($listAdverts) / $nbPerPage);
+       
+       // Si la page n'existe pas, on retourne une 404
+       if ($page -> $nbPages) {
+           throw $this->createNotFoundException("La page ".$page." n'existe pas");
+       }
       
+    // Affichage
        return $this->render('BONBERSPlatformBundle:Advert:index.html.twig', array(
-           'listAdverts' => $listAdverts
+           'listAdverts' => $listAdverts,
+           'nbPages'   => $nbPages,
+           'page'    => $page,
            ));
     }
 
     public function viewAction($id)
     {
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony2',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-      'date'    => new \Datetime()
-    );
-
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        // On récupère une seule annonce avec la méthode find($id)
+    $advert = $em->getRepository('BONBERSPlatformBundle:Advert')->find($id);
+    
+    // $advert est une instance de BONBERS\PlatformBundle\Entity\Advert
+    // null si l'id $id n'existe pas, d'où le if :
+    if (null === $advert) {
+        throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+    
+    // Récupération de la liste des candidatures de l'annonce
+    $listApplications = $em
+            ->getRepository('BONBERSPlatformBundle:Application')
+            ->findBy(array('advert' => $advert))
+            ;
+    
+    // Récupération des AdvertSkill de l'annonce
+    $listAdvertSkills = $em
+            ->getRepository('BONBERSPlatformBundle:AdvertSkill')
+            ->findBy(array('advert' => $advert))
+            ;
+   
     return $this->render('BONBERSPlatformBundle:Advert:view.html.twig', array(
-      'advert' => $advert
-
+      'advert'              => $advert,
+      'listApplications' => $listApplications,
+      'listAdvertSkills'  => $listAdvertSkills, 
     ));
     }
     
     public function menuAction($limit)
     {
-        $listAdverts = array(
-      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
-      array('id' => 5, 'title' => 'Mission de webmaster'),
-      array('id' => 9, 'title' => 'Offre de stage webdesigner')
-        );
+        $em = $this->getDoctrine->getManager();
+       
+        $listAdverts = $em->getRepository('BONBERSPlatformBundle:Advert')->findBy(
+                array(),                           // Pas de critère
+                array('date' => 'desc'),   // On trie par date décroissante
+                $limit,                            // On sélectionne $limit annonce
+                0                                   // A partir du premier
+                );
+                
         return $this->render('BONBERSPlatformBundle:Advert:menu.html.twig', array(
-      // Tout l'intérêt est ici : le contrôleur passe
-      // les variables nécessaires au template !
       'listAdverts' => $listAdverts
     ));
     }
     public function addAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        
         if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice','Annonce bien enregistrée.');
-            return $this->redirectToRoute('bonbers_platform_view', array('id' => 5));
+            return $this->redirectToRoute('bonbers_platform_view', array('id' => $advert->getId()));
         }
         return $this->render('BONBERSPlatformBundle:Advert:add.html.twig');
     }
 
     public function editAction($id, Request $request)
     {
-        if ($request->isMethod('POST')) {
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifié');
-           return $this->redirectToRoute('bonbers_platform_view', array('id' => 5));
+        $em = $this->getDoctrine()->getManager();
+        
+        $advert = $em->getRepository('BONBERSPlatformBundle:Advert')->find($id);
+        
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas");
         }
         
-            $advert = array(
-           'title' => 'Recherche développeur Symfony',
-           'id'    => $id,
-           'author' => 'Alexandre',
-           'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon..',
-           'date' => new \Datetime()
-       );
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifié');
+           return $this->redirectToRoute('bonbers_platform_view', array('id' => $advert->getId()));
+        }
        
        return $this->render('BONBERSPlatformBundle:Advert:edit.html.twig', array(
            'advert' => $advert
@@ -99,6 +131,22 @@ class AdvertController extends Controller
     
     public function deleteAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+        
+        $advert = $em->getRepository('BONBERSPlatformBundle:Advert')->find($id);
+        
+        if(null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+        
+        // Boucle sur les catégories de l'annonce pour les supprimer
+        
+        foreach ($advert->getCategories() as $category) {
+            $advert->removeCategory($category);
+        }
+        
+        $em->flush();
+        
         return $this->render('BONBERSPlatformBundle:Advert:delete.html.twig');
     }
    
---
+++ src/BONBERS/PlatformBundle/Entity/Advert.php~
@@ -0,0 +1,284 @@
+<?php
+
+namespace BONBERS\PlatformBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Advert
+ *
+ * @ORM\Table(name="advert")
+ * @ORM\Entity(repositoryClass="BONBERS\PlatformBundle\Repository\AdvertRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Advert
+{
+    
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(name="slug", type="string", length=255, unique=true)
+     */
+    private $slug;
+    
+    /**
+     * @ORM\OneToOne(targetEntity="BONBERS\PlatformBundle\Entity\Image", cascade={"persist"})
+     */ 
+    private $image;
+
+    /**
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+    
+    public function updateDate()
+    {
+        $this->setUpdatedAt(new \Datetime());
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+   
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Advert
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Advert
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Advert
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Advert
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+    
+    /**
+     * @ORM\Column(name="published", type="boolean")
+     */
+    private $published = true;
+
+    /**
+     * Set published
+     *
+     * @param boolean $published
+     *
+     * @return Advert
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return boolean
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \BONBERS\PlatformBundle\Entity\Image $image
+     *
+     * @return Advert
+     */
+    public function setImage(\BONBERS\PlatformBundle\Entity\Image $image = null)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \BONBERS\PlatformBundle\Entity\Image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Advert
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Advert
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
